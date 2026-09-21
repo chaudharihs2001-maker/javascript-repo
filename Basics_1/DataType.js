4f5cb51364cd06c0d11867eb1c04c761727da59f
chaudharihs2001-maker <chaudharihs2001@gmail.com>
2026-09-21 15:10:24 +0000

    DataTypes

---
+++ Basics_1/DataType.js
@@ -0,0 +1,17 @@
+let name = "Hemant" // string 
+let age = 25        // number 
+let loggedIn = false  // boolean
+
+console.log(typeof age); // return type[ number]  
+console.log(typeof null); // return object
+console.log(typeof undefined); //return undefined
+// primative datatype
+// number => 2 to power 53
+// bignit => large number 
+// string => ' '
+// boolen => true / false 
+// null => standalone value  empty value
+// undefined => value is not defined not assign 
+let state = null
+// symbol => Use in React component define unquieness
+  
