b9d1b57b3590257cc836ce9a42200c0824467893
chaudharihs2001-maker <chaudharihs2001@gmail.com>
2026-09-21 15:48:57 +0000

    datatype coversion

--- Basics_1/DataType.js
+++ Basics_1/DataType.js
@@ -14,4 +14,33 @@ console.log(typeof undefined); //return undefined
 // undefined => value is not defined not assign 
 let state = null
 // symbol => Use in React component define unquieness
-  
+
+// datatypes Converison 
+
+let score = "33adf" // covert it to number 
+// let score = null //  return 0
+// let score = undefined // return NaN
+// let score = true // return 1 
+console.log(typeof score);
+console.log(typeof (score)); // return string 
+
+// conversion  
+let NumberValue = Number(score);
+console.log(typeof (score));
+console.log(NumberValue) // return NaN not a number
+
+
+let loggedata = 0
+
+let covert = Boolean (loggedata) // return true /false
+console.log(covert);
+
+let stringdata = 33
+
+let covertstring = string (stringdata) // return string to check used typeof
+console.log(covertstring);
+console.log(tyepeof (covertstring)) // return string 
+let string = "  " // empty string return false 
+let string = "Javascript" // return true
+
+
